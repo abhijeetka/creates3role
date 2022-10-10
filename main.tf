b8c9fb36f38bbc523a4262d874922c317b6ea36c
Abhijeet Kamble <akamble@calibo.com>
2022-10-10 17:23:08 +0530

    fix

--- main.tf
+++ main.tf
@@ -43,7 +43,6 @@ resource "aws_iam_policy" "aws-s3-policy" {
       {
         "Sid": "PublicRead",
         "Effect": "Allow",
-        "Principal": "*",
         "Action": [
           "s3:GetObject",
           "s3:GetObjectVersion",
